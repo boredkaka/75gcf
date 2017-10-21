abb977698fb4be48121ba49fe73f97ea2da69ef7
Karolini Rosine <karolini.rosine@gmail.com>
2017-10-21 15:56:44 -0200

    Adicionado funcionalidade de fatorial

--- src/Main.java
+++ src/Main.java
@@ -12,11 +12,15 @@ public class Main {
         int opcao;
 
         do {
-            System.out.println("MENU:\n[1] Tabuada \n[0] Sair");
+            System.out.println("MENU:\n" +
+                    "[1] Tabuada \n" +
+                    "[2] Fatorial \n" +
+                    "[0] Sair");
             opcao = s.nextInt();
 
             switch (opcao) {
                 case 1: tabuada(); break;
+                case 2: fatorial(); break;
             }
         } while(opcao != 0);
     }
@@ -32,5 +36,17 @@ public class Main {
         for (int i = 1; i <= 10; i++) {
             System.out.println(num + "x" + i + "=" + num*i);
         }
+    }
+
+    public void fatorial() {
+        System.out.println("Informe um número");
+        int num = s.nextInt();
+
+        int fat = 1;
+        for( int i = 2; i <= num; i++ ) {
+            fat *= i;
+        }
+
+        System.out.println("O fatorial de " + num + " é " + fat);
     }
 }
